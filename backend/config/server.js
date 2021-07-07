b80bf252979eb039e55e66e70397603adbed9bd1
zokr6466 <zokrimahamadjonov29@gmail.com>
2021-07-07 03:36:41 -0700

    zokir

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e348d621a965632c1bbf5666786885f6'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0a1855d7-c15a-435d-85bb-2d4efe503c8b'
+};
